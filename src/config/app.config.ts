22f3301622a0b2cd865a7ca87f9a86b0c8ab9826
Lev <thegoldcat5@gmail.com>
2025-04-28 01:41:07 +0000

    fix: swagger docs fixed

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,17 +1,17 @@
 import { registerAs } from '@nestjs/config';
-import { getOrDefault, getOrThrow } from './utils';
+import env from 'env-var';
 
 export const APP_CONFIG_KEY = 'app-config';
 
 export const appConfig = registerAs(APP_CONFIG_KEY, () => ({
-  DB_HOST: getOrThrow('DB_HOST'),
-  DB_PORT: parseInt(getOrThrow('DB_PORT')),
-  DB_USERNAME: getOrThrow('DB_USERNAME'),
-  DB_PASSWORD: getOrThrow('DB_PASSWORD'),
-  DB_DATABASE: getOrThrow('DB_DATABASE'),
-  DB_SYNC: getOrDefault('DB_SYNC', 'false') === 'true' ? true : false, // Should be false in production
+  DB_HOST: env.get('DB_HOST').required().asString(),
+  DB_PORT: env.get('DB_PORT').required().asPortNumber(),
+  DB_USERNAME: env.get('DB_USERNAME').required().asString(),
+  DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
+  DB_DATABASE: env.get('DB_DATABASE').required().asString(),
+  DB_SYNC: env.get('DB_SYNC').default('false').asBool(),
 
-  PORT: parseInt(getOrDefault('PORT', '3000')),
-  JWT_SECRET: getOrThrow('JWT_SECRET'),
-  ENV: getOrDefault('NODE_ENV', 'development'),
+  PORT: env.get('PORT').default('3000').asPortNumber(),
+  SERVER_URL: env.get('SERVER_URL').default('http://localhost').asUrlString(),
+  JWT_SECRET: env.get('JWT_SECRET').required().asString(),
 }));
--- src/swagger.ts
+++ src/swagger.ts
@@ -3,26 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { APP_CONFIG_KEY } from './config/app.config';
 
-const pickServer = (env: string, port: number) => {
-  if (env === 'production') {
-    return ['https://stage.sqwoz-hrov.ru/payments', 'Sqwoz Staging'];
-  }
-
-  return [`http://localhost:${port}/`, 'Local server'];
-};
-
 export const buildSwaggerDocs = (
   app: INestApplication,
   configService: ConfigService,
 ) => {
-  const containerPort = configService.get(`${APP_CONFIG_KEY}.PORT`);
-  const env = configService.get(`${APP_CONFIG_KEY}.ENV`);
-  const [serverUrl, serverDescription] = pickServer(env, containerPort);
+  const serverUrl = configService.get(`${APP_CONFIG_KEY}.SERVER_URL`);
 
   const config = new DocumentBuilder()
     .setTitle('Skvoz Payment System')
     .setDescription('API documentation for the Skvoz Payment System')
-    .addServer(serverUrl, serverDescription)
+    .addServer(serverUrl)
     .addBearerAuth()
     .setVersion('0.0.2')
     .addTag('Payments')
