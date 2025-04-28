aaa3444a45bf021572fb26a78d8d2dfc472fbd9a
Lev <thegoldcat5@gmail.com>
2025-04-28 01:49:42 +0000

    fix: env-var import fix

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,17 +1,17 @@
 import { registerAs } from '@nestjs/config';
-import env from 'env-var';
+import { get } from 'env-var';
 
 export const APP_CONFIG_KEY = 'app-config';
 
 export const appConfig = registerAs(APP_CONFIG_KEY, () => ({
-  DB_HOST: env.get('DB_HOST').required().asString(),
-  DB_PORT: env.get('DB_PORT').required().asPortNumber(),
-  DB_USERNAME: env.get('DB_USERNAME').required().asString(),
-  DB_PASSWORD: env.get('DB_PASSWORD').required().asString(),
-  DB_DATABASE: env.get('DB_DATABASE').required().asString(),
-  DB_SYNC: env.get('DB_SYNC').default('false').asBool(),
+  DB_HOST: get('DB_HOST').required().asString(),
+  DB_PORT: get('DB_PORT').required().asPortNumber(),
+  DB_USERNAME: get('DB_USERNAME').required().asString(),
+  DB_PASSWORD: get('DB_PASSWORD').required().asString(),
+  DB_DATABASE: get('DB_DATABASE').required().asString(),
+  DB_SYNC: get('DB_SYNC').default('false').asBool(),
 
-  PORT: env.get('PORT').default('3000').asPortNumber(),
-  SERVER_URL: env.get('SERVER_URL').default('http://localhost').asUrlString(),
-  JWT_SECRET: env.get('JWT_SECRET').required().asString(),
+  PORT: get('PORT').default('3000').asPortNumber(),
+  SERVER_URL: get('SERVER_URL').default('http://localhost').asUrlString(),
+  JWT_SECRET: get('JWT_SECRET').required().asString(),
 }));
